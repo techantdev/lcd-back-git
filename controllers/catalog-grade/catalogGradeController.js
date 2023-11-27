53559132c586d408d0ddce4f5c77b8dadca046d7
Cristian Andrade <cristian@appmelo.com>
2023-11-27 14:48:54 -0500

    added routes logic

---
+++ controllers/catalog-grade/catalogGradeController.js
@@ -0,0 +1,9 @@
+const createCatalogGrade = require('./createCatalogGrade');
+const deleteCatalogGrade = require('./deleteCatalogGrade');
+const getCatalogGrade = require('./getCatalogGrade');
+const updateCatalogGrade = require('./updateCatalogGrade');
+
+exports.createCatalogGrade = null;
+exports.deleteCatalogGrade = null;
+exports.getCatalogGrade = null;
+exports.updateCatalogGrade = null;
---
+++ routes/catalogGradeRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const catalogGradeController = require('./../controllers/catalog-grade/catalogGradeController');
+
+const router = express.Router();
+
+router.route('').get(catalogGradeController.getCatalogGrade).post(catalogGradeController.createCatalogGrade);
+
+router.route('/:catalogGradeId').patch(catalogGradeController.updateCatalogGrade).delete(catalogGradeController.deleteCatalogGrade);
+
+module.exports = router;
