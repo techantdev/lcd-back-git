7e2c0dea085bfe9d6f7b3e50eb1ae4749f87c1b6
MangelG9 <miguelagp3009@gmail.com>
2024-02-26 10:51:52 -0500

    add correction teacherSubjectCourseController

--- src/controllers/teacher-subject-course/teacherSubjectCourseController.ts
+++ src/controllers/teacher-subject-course/teacherSubjectCourseController.ts
@@ -6,11 +6,12 @@ import getTeacherSubjectCourseMethod from './getTeacherSubjectCourse';
 
 import { catchAsync } from '../../middleware/middleware';
 
-export const createTeacherSubjectCourse = catchAsync;
-(req: Request) =>
-  createTeacherSubjectCourseMethod(req.body.teacherId, req.body.academicYearId, req.body.yearSubjectId, req.body.courseId);
-export const deleteTeacherSubjectCourse = catchAsync;
-(req: Request<{}, {}, {}, { teacherSubjectCourseIds: String }>) => deleteTeacherSubjectCourseMethod(req.query.teacherSubjectCourseIds);
-export const getTeacherSubjectCourse = catchAsync;
-(req: Request<{}, {}, {}, { teacherId: String; academicYearId: String }>) =>
-  getTeacherSubjectCourseMethod(req.query.teacherId, req.query.academicYearId);
+export const createTeacherSubjectCourse = catchAsync((req: Request) =>
+  createTeacherSubjectCourseMethod(req.body.teacherId, req.body.academicYearId, req.body.yearSubjectId, req.body.courseId)
+);
+export const deleteTeacherSubjectCourse = catchAsync((req: Request<{}, {}, {}, { teacherSubjectCourseIds: String }>) =>
+  deleteTeacherSubjectCourseMethod(req.query.teacherSubjectCourseIds)
+);
+export const getTeacherSubjectCourse = catchAsync((req: Request<{}, {}, {}, { teacherId: String; academicYearId: String }>) =>
+  getTeacherSubjectCourseMethod(req.query.teacherId, req.query.academicYearId)
+);
